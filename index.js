ab5f803b23f5435dc7f63639d4a051a845e1ff29
Wendy Cordero <wcconsulting11@gmail.com>
2020-08-29 21:43:59 +0000

    resolved sound and ball motion

--- index.js
+++ index.js
@@ -11,24 +11,24 @@ window.addEventListener("load",() => {
         "#800080"
     ];
 
-        //the sounds functions
+        //the sounds functions//
     pads.forEach((pad, index) => {
-        pad.addEventListener.addEventListener("click", function() {
+        pad.addEventListener("click", function() {
             sounds[index].currentTime = 0;
             sounds[index].play();
-            createBubble(index); 
-        });
-        
+
+            createBubbles(index); 
+        }); 
     });
-        //creates the bubbles
-    const createBubble = index => {
+        //creates the bubbles//
+    const createBubbles = index => {
+        const bubble = document.createElement("div");
         visual.appendChild(bubble);
         bubble.style.backgroundColor = colors[index];
-        bubble.style.annimation = 'jump 1s ease';
+        bubble.style.animation = 'jump 1s ease';
         bubble.addEventListener("animationend", function() {
             visual.removeChild(this);
         });
-    };
-    
+    }; 
 
 });
